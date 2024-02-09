6d824579259600f24698492a94f1c88aafad9496
parshwa282011 <parshwashah282011@gmail.com>
2024-02-08 17:05:49 -0800

    testing

--- Main.js
+++
@@ -1,9 +0,0 @@
-function button(){
-    tag = document.getElementById("tag");
-    text = tag.innerHTML;
-    if (text = "LOL2"){
-        tag.innerHTML = "ItWorked";
-    } else {
-        tag.innerHTML = "LOL2";
-    }
-}
