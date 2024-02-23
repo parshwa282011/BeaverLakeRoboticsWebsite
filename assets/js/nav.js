424aa6ae0f773db2ecd9b3f88380930a8d412df2
Parshwa <parshwashah282011@gmail.com>
2024-02-22 23:21:37 -0800

    nav bar update a bit left to be complete

---
+++ assets/js/nav.js
@@ -0,0 +1,23 @@
+function loadNavbar() {
+    elmnt = document.getElementById("nav-placeholder")
+    xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4) {
+		    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+	    	if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+    	}	
+	}
+	console.log(xhttp.open("GET", WEB_HOME+"assets/nav/nav.html", true));
+    xhttp.send();
+	var listOfLinks = document.getElementsByTagName("a");
+	i=0;
+	while (i < listOfLinks.length){
+		var a = listOfLinks[i];
+		try {
+			a.href = WEB_HOME + a.href;
+		} catch (error) {
+			continue
+		}
+		i++;
+	}
+}
